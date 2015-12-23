5f540c8cdf0e34e6ace1ba3261413b3cb7770a43
Erik Harlefelt <me@iterik.se>
2015-12-23 17:45:11 +0100

    Creat

---
+++ ChristmasTree/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int space = 10;
+            int asterix = 1;
+
+            for(int i = 0; i < 10; i++)
+            {
+                for(int ii = 0; ii < space; ii++)
+                {
+                    Console.Write(" ");
+                }
+
+                for(int ii = 0; ii < asterix; ii++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+                asterix++;
+                space--;
+            }
+
+            Console.WriteLine("         |||");
+            Console.WriteLine("         |||");
+
+            Console.ReadLine();
+        }
+    }
+}
